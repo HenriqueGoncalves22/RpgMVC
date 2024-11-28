dee59bfd152897423d6896257a364021c6a3e1c2
Henrique Gonçalves <henrique.gonc.gamer@gmail.com>
2024-11-27 22:42:45 -0300

    Criacao do front do RpgApi

---
+++ Models/Enuns/ClasseEnum.cs
@@ -0,0 +1,9 @@
+namespace RpgMVC.Models.Enuns
+{
+    public enum ClasseEnum
+    {
+        Cavaleiro = 1,
+        Mago = 2,
+        Clerigo = 3
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace RpgMVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
